b21dd06cdfa9ff447cd531d95be03d83a20c51d5
Frankrochha7 <frank.procha@senacsp.edu.br>
2026-09-02 10:05:00 -0300

    Add files via upload

---
+++ exercicio1.java
@@ -0,0 +1,12 @@
+
+
+public class exercicio1 {
+    
+public static void main(String[] args) {
+
+System.out.println("O seu nome completo é? Frank Pereira da Rocha");
+System.out.println("O seu Registro Acadêmico RA é? 1143547935");
+System.out.println("O seu curso é? Analise e desenvolvimento de sistemas");
+}
+
+}
